89119660b54cdb84d8f82b901824151d3c271abb
Chao Liang <52976469+Chao0813@users.noreply.github.com>
2020-02-13 16:02:35 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Label, Input } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      reverseText: "",
+      capsText: ""
+    };
+  }
+  handleChange = (context, element) => {
+    const text = element.value;
+    const reverseText = text.split("").reverse().join("");
+    const capsText = text.toUpperCase();
+    this.setState({ text: text, reverseText: reverseText, capsText: capsText });
+  };
+
+  render() {
+    return (
+      <div>
+        <Input onChange={this.handleChange} />
+        <Label style={{ marginLeft: "8rem" }}>{this.state.reverseText}</Label>
+        &nbsp;
+        &nbsp;
+        <Label style={{ marginLeft: "2rem", marginTop: "30rem" }}>
+          {this.state.capsText}
+        </Label>
+        &nbsp;
+        &nbsp;
+        <Label style={{ fontSize: "3rem" }}>{this.state.text}</Label>
+      </div>
+    );
+  }
 }
 
 export default App;
